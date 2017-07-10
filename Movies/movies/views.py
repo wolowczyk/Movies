6413fb72bb16ba77ec0d697ca2a3b257026fc1ed
Pawel Wolowczyk <pawelwolowczyk@gmail.com>
2017-07-11 00:14:54 +0200

    added put

--- Movies/movies/views.py
+++ Movies/movies/views.py
@@ -38,3 +38,11 @@ class MovieView(APIView):
         movie = self.get_object(id)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id, format=None):
+        movie = self.get_object(id)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
