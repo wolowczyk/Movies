7db5169d5dd77594c3f0e7af8ab167c7415c652b
Pawel Wolowczyk <pawelwolowczyk@gmail.com>
2017-07-10 15:44:35 +0200

    made relations between models

--- Movies/movies/models.py
+++ Movies/movies/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=81)
     nationality = models.CharField(max_length=81)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=81)
+    description = models.TextField(default="opis")
+    director = models.ForeignKey(Person)
+    year = models.IntegerField()
+    actors = models.ManyToManyField(Person, through='Role', related_name='playing')
+
+
+class Role(models.Model):
+    character = models.CharField(max_length=81)
+    movie = models.ForeignKey(Movie)
+    actor = models.ForeignKey(Person)
